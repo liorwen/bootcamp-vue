c2fb1de439cc2582dfbf54d65a4fcb5358dbf6a8
liorwen <lyndonwen@degather.com>
2016-08-08 15:49:36 +0800

    Add foot bar (#8)
    * Add FootBar component
    
    * Fix App style
    
    * Fix footer style

---
+++ resources/assets/js/todo_list/components/FootBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="footer">
+        <h3>This is Footer Test</h3>
+    </div>
+</template>
+<style>
+   .footer {
+       border-top: 6px solid red;
+   }
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
