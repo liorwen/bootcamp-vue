ee6d9087df6493d567c5f0d4a6144d03298c33e2
lyndon <lyndonwen@degather.com>
2016-08-08 14:05:33 +0800

    Add header-bar component

---
+++ resources/assets/js/todo_list/components/HeaderBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="menu-links">
+        <a v-link="{path:'/'}">HOME</a>
+        <a v-link="{path:'/todolist'}">Todo List</a>
+    </div>
+</template>
+<style>
+    .menu-links a {
+        display: inline-block;
+        text-decoration: none;
+        color: #555;
+        margin-right: 30px;
+    }
+</style>
+<script>
+    import HeaderComponent from './components/header.vue'
+    import OtherComponent from './components/other.vue'
+    export default{
+        data(){
+            return{
+                name: 'Matt Test'
+            }
+        }
+    }
+</script>
