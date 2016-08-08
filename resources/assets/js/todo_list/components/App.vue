0c70a6f9745a01c28c5e6f215b6d737c44091afe
lyndon <lyndonwen@degather.com>
2016-08-08 14:07:30 +0800

    Add header-bar in App view

--- resources/assets/js/todo_list/components/App.vue
+++ resources/assets/js/todo_list/components/App.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <header-bar></header-bar>
+    </div>
+    <div>
         <todo-component></todo-component>
     </div>
 </template>
@@ -7,9 +10,11 @@
 
 </style>
 <script>
+    import HeaderBar from './HeaderBar.vue'
     import TodoComponent from './TodoComponent.vue';
     export default{
         components:{
+            HeaderBar
             TodoComponent
         }
     }
