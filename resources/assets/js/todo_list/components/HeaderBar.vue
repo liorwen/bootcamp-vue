7cb9c5d5d5320d7e6f273ca6f496ee1b52e3d693
liorwen <lyndonwen@degather.com>
2016-08-08 14:26:01 +0800

    Add header bar (#2)
    * Add header-bar component
    
    * Add header-bar in App view
    
    * Fix HeaderBar
    
    * Fix headerbar link
    
    * Fix HeaderBar Language

---
+++ resources/assets/js/todo_list/components/HeaderBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="menu-links">
+        <a href="/">首頁</a>
+        <a href="/todolist">事項列表</a>
+    </div>
+</template>
+<style>
+    .menu-links a {
+        display: inline-block;
+        text-decoration: none;
+        color: #555;
+        margin-right: 30px;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                name: 'Matt Test'
+            }
+        }
+    }
+</script>
