557131eb8c745f53038e4626c41cb490ff61b33e
lyndon <lyndonwen@degather.com>
2016-08-08 15:39:55 +0800

    Fix App style

--- resources/assets/js/todo_list/components/App.vue
+++ resources/assets/js/todo_list/components/App.vue
@@ -1,21 +1,35 @@
 <template>
-    <div>
+    <div id="header">
         <header-bar></header-bar>
     </div>
-    <div>
+    <div id="main">
         <todo-component></todo-component>
+    </div>
+    <div id="footer">
+        <foot-bar></foot-bar>
     </div>
 </template>
 <style>
-
+    #main {
+        padding-bottom: 100px;
+        margin-top: 30px;
+        height: 600px;
+    }
+    #footer {
+        height: 100px;
+        position: relative;
+        margin-top: -100px;
+    }
 </style>
 <script>
     import HeaderBar from './HeaderBar.vue'
     import TodoComponent from './TodoComponent.vue';
+    import FootBar from './FootBar.vue';
     export default{
         components:{
             HeaderBar,
-            TodoComponent
+            TodoComponent,
+            FootBar
         }
     }
 </script>
