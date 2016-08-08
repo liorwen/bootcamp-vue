fad916ee4559f4ea70800fc2a5004e3c4673767a
lyndon <lyndonwen@degather.com>
2016-08-08 15:31:18 +0800

    Add FootBar component

---
+++ resources/assets/js/todo_list/components/FootBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footer">
+        <h3>This is Footer Test</h3>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
