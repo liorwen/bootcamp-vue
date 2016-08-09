acb70637bdd0c82a0a6096c9b0fc05989cbe8ac1
liorwen <lyndonwen@degather.com>
2016-08-09 11:43:45 +0800

    Fix TodoComponent style (#10)
    * Add FootBar component
    
    * Fix App style
    
    * Fix footer style
    
    * Fix Todo style

--- resources/assets/js/todo_list/components/TodoComponent.vue
+++ resources/assets/js/todo_list/components/TodoComponent.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="todo-title">{{ title }}</div>
-
-    <div class="todo-name">{{ name }}</div>
+    <div class="container">
+        <div class="title">{{ title }}</div>
+        <div class="nickname">{{ nickname }}</div>
+        <div class="msg">{{msg}}</div>
+    </div>
 </template>
 
 <script>
@@ -9,20 +11,31 @@
         data () {
             return {
                 title: 'Add Title',
-                name: 'Matt Test'
+                nickname: 'Matt Test',
+                msg: 'This is Test msg'
             };
         }
     };
 </script>
-
 <style>
-      .todo-title {
-          font-size: 18px;
-          color: #9c0033;
-          border-bottom: 6px solid green;
-      }
-      .todo-name {
-          padding: 4px;
-          font-size: 12px;
-      }
+    .container {
+        width: 300px;
+        height: 150px;
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 20px;
+    }
+    .title {
+        font-size: 18px;
+        color: #9c0033;
+        border-bottom: 6px solid green;
+    }
+    .nickname {
+        padding: 4px;
+        font-size: 12px;
+    }
+    .msg {
+        padding: 4px;
+        font-size: 16px;
+    }
 </style>
